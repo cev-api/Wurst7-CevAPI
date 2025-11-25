d858761484300500729631db1c02f2618afbc100
cev-api <215493617+cev-api@users.noreply.github.com>
2025-11-25 10:07:36 +1000

    NoSlowDown Update

--- src/main/java/net/wurstclient/hacks/NoSlowdownHack.java
+++ src/main/java/net/wurstclient/hacks/NoSlowdownHack.java
@@ -9,15 +9,73 @@ package net.wurstclient.hacks;
 
 import net.wurstclient.Category;
 import net.wurstclient.SearchTags;
+import net.wurstclient.events.IsPlayerInLavaListener;
+import net.wurstclient.events.IsPlayerInLavaListener.IsPlayerInLavaEvent;
+import net.wurstclient.events.VelocityFromFluidListener;
+import net.wurstclient.events.VelocityFromFluidListener.VelocityFromFluidEvent;
 import net.wurstclient.hack.Hack;
+import net.wurstclient.settings.CheckboxSetting;
 
 @SearchTags({"no slowdown", "no slow down"})
 public final class NoSlowdownHack extends Hack
+	implements IsPlayerInLavaListener, VelocityFromFluidListener
 {
+	private final CheckboxSetting lavaSpeed = new CheckboxSetting(
+		"No lava slowdown", "Removes lava movement penalties.\n"
+			+ "Some servers treat this like a speedhack.",
+		false);
+	
+	private boolean bypassingLava;
+	
 	public NoSlowdownHack()
 	{
 		super("NoSlowdown");
 		setCategory(Category.MOVEMENT);
+		addSetting(lavaSpeed);
+	}
+	
+	@Override
+	protected void onEnable()
+	{
+		EVENTS.add(IsPlayerInLavaListener.class, this);
+		EVENTS.add(VelocityFromFluidListener.class, this);
+	}
+	
+	@Override
+	protected void onDisable()
+	{
+		EVENTS.remove(IsPlayerInLavaListener.class, this);
+		EVENTS.remove(VelocityFromFluidListener.class, this);
+		bypassingLava = false;
+	}
+	
+	@Override
+	public void onIsPlayerInLava(IsPlayerInLavaEvent event)
+	{
+		if(!lavaSpeed.isChecked())
+		{
+			bypassingLava = false;
+			return;
+		}
+		
+		if(event.isNormallyInLava())
+		{
+			bypassingLava = true;
+			event.setInLava(false);
+			return;
+		}
+		
+		bypassingLava = false;
+	}
+	
+	@Override
+	public void onVelocityFromFluid(VelocityFromFluidEvent event)
+	{
+		if(!lavaSpeed.isChecked() || !bypassingLava)
+			return;
+		
+		if(event.getEntity() == MC.player)
+			event.cancel();
 	}
 	
 	// See BlockMixin.onGetVelocityMultiplier() and
