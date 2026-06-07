9cda9d09b88df9939457bbc9496a553d51d83bf2
Cev-API <215493617+cev-api@users.noreply.github.com>
2026-06-08 01:40:30 +1000

    Improve PearlESP Owner Identification

---
+++ src/main/java/net/wurstclient/mixin/MobEntityMixin.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2014-2026 Wurst-Imperium and contributors.
+ *
+ * This source code is subject to the terms of the GNU General Public
+ * License, version 3. If a copy of the GPL was not distributed with this
+ * file, You can obtain one at: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+package net.wurstclient.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.world.entity.Mob;
+import net.wurstclient.WurstClient;
+
+@Mixin(Mob.class)
+public abstract class MobEntityMixin
+{
+	@Inject(method = "isSaddled", at = @At("HEAD"), cancellable = true)
+	private void forceSaddled(CallbackInfoReturnable<Boolean> cir)
+	{
+		if(WurstClient.INSTANCE.getHax().entityControlHack
+			.shouldEnforceSaddled())
+			cir.setReturnValue(true);
+	}
+	
+	@Inject(method = "getJumpPower",
+		at = @At("RETURN"),
+		cancellable = true,
+		require = 0)
+	private void forceJumpStrength(CallbackInfoReturnable<Float> cir)
+	{
+		if(!WurstClient.INSTANCE.getHax().entityControlHack
+			.shouldEnforceJumpStrength())
+			return;
+		
+		cir.setReturnValue(Math.max(cir.getReturnValueF(), 0.7F));
+	}
+}
