b12050d474f1c4c9a4794eac03c619dedb72d160
Cev-API <215493617+cev-api@users.noreply.github.com>
2026-07-01 00:49:23 +1000

    Fix NoBackground & Autofly

--- src/main/java/net/wurstclient/mixin/ScreenMixin.java
+++ src/main/java/net/wurstclient/mixin/ScreenMixin.java
@@ -11,21 +11,22 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.components.events.AbstractContainerEventHandler;
 import net.minecraft.client.gui.screens.Screen;
 import net.wurstclient.WurstClient;
 
-@Mixin(Screen.class)
+@Mixin(value = Screen.class, remap = false)
 public abstract class ScreenMixin extends AbstractContainerEventHandler
 	implements Renderable
 {
 	@Inject(
-		method = "renderTransparentBackground(Lnet/minecraft/client/gui/GuiGraphics;)V",
+		method = "extractBackground(Lnet/minecraft/client/gui/GuiGraphicsExtractor;IIF)V",
 		at = @At("HEAD"),
 		cancellable = true)
-	public void onRenderInGameBackground(GuiGraphics context, CallbackInfo ci)
+	public void onExtractBackground(GuiGraphicsExtractor context, int mouseX,
+		int mouseY, float partialTicks, CallbackInfo ci)
 	{
 		if(WurstClient.INSTANCE.shouldHideWurstUiMixins())
 			return;
