d72d094ccfd9fc75a3560b139f2f89e7e53ff9c3
Cev-API <215493617+cev-api@users.noreply.github.com>
2026-08-09 13:32:27 +1000

    Remove Upstreams Update Checker

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -192,7 +192,8 @@ public enum WurstClient
 		eventManager.add(PostMotionListener.class, rotationFaker);
 		
 		updater = new WurstUpdater();
-		eventManager.add(UpdateListener.class, updater);
+		// Disabled upstream's update checker. Because well, you're using CEVAPI ain't cha?
+		// eventManager.add(UpdateListener.class, updater); 
 		
 		forkUpdateChecker = new ForkUpdateChecker();
 		eventManager.add(UpdateListener.class, forkUpdateChecker);
